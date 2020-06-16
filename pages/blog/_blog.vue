52885b5046e86a73856c14c890e1f34be7e38b13
Jorge Raboso Barceló <>
2020-06-16 15:23:15 +0200

    commit

---
+++ pages/blog/_blog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) return { blogPost: payload };
+    else
+      return {
+        blogPost: await require(`~/assets/content/blog/${params.blog}.json`),
+      };
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div class="c-posts">
+    <div class="c-post" v-for="post in blogPosts" :key="post.id">
+      <div class="c-post__title">{{post.title}}</div>
+      <div class="c-post__name"></div>
+      <div class="c-post__body" v-html="post.body">
+
+      </div>
+    </div>
+   {{blogPosts}}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
